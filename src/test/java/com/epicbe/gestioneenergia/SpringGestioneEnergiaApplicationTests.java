fd0bfe292ad33dec061f9ea6a3bf0c197f4c9597
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-02 11:11:35 +0200

    add jwt secret, fix test annotation import

--- src/test/java/com/epicbe/gestioneenergia/SpringGestioneEnergiaApplicationTests.java
+++ src/test/java/com/epicbe/gestioneenergia/SpringGestioneEnergiaApplicationTests.java
@@ -1,6 +1,6 @@
 package com.epicbe.gestioneenergia;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.extern.slf4j.Slf4j;
