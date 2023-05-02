cfc3eac18c0cd37700f30acd0e0bfc48bb3516ad
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-05-02 22:01:12 +0200

    comuni repo & service

--- src/main/java/com/epicbe/gestioneenergia/repository/ComuneRepository.java
+++ src/main/java/com/epicbe/gestioneenergia/repository/ComuneRepository.java
@@ -1,10 +1,21 @@
 package com.epicbe.gestioneenergia.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.epicbe.gestioneenergia.model.Comune;
 
-public interface ComuneRepository extends CrudRepository<Comune, Long>, PagingAndSortingRepository<Comune, Long> {
+public interface ComuneRepository 
+extends CrudRepository<Comune, Long>, PagingAndSortingRepository<Comune, Long> {
+
+	Page<Comune> findByCodProvinciaPage(Long codiceProvincia, Pageable pageable);
+	Page<Comune> findByNomePage(String nome, Pageable pageable);
+	Page<Comune> findByProvinciaPage(String provincia, Pageable pageable);
+	List<Comune> findByCodProvincia(Long codiceProvincia);
+	Comune findByNome(String nome);
 
 }
--- src/main/java/com/epicbe/gestioneenergia/service/ComuneService.java
+++ src/main/java/com/epicbe/gestioneenergia/service/ComuneService.java
@@ -1,8 +1,35 @@
 package com.epicbe.gestioneenergia.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import com.epicbe.gestioneenergia.model.Comune;
+import com.epicbe.gestioneenergia.repository.ComuneRepository;
 
 @Service
 public class ComuneService {
+	
+	@Autowired
+	ComuneRepository comuneRepo;
+	
+	public List<Comune> getAllComuni() {
+		return (List<Comune>) comuneRepo.findAll();
+	}
+	
+	public List<Comune> findByCodProvincia(Long codiceProvincia){
+		return (List<Comune>) comuneRepo.findByCodProvincia(codiceProvincia);
+	}
+	
+	public Comune findByNome(String nome) {
+		return (Comune) comuneRepo.findByNome(nome);
+	}
+	
+	public Page<Comune> getAllComuniPageable(Pageable pageable) {
+		return (Page<Comune>) comuneRepo.findAll(pageable);
+	}
 
 }
