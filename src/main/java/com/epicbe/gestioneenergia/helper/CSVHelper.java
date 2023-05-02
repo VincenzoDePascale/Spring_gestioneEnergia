20256d1c9c8f2e3454da6ed23c922c11674936e6
Vincenzo M <vincenzomaiorana@gmail.com>
2023-05-02 17:02:01 +0200

    fix inputstream

--- src/main/java/com/epicbe/gestioneenergia/helper/CSVHelper.java
+++ src/main/java/com/epicbe/gestioneenergia/helper/CSVHelper.java
@@ -11,54 +11,58 @@ import java.util.List;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-
 import org.springframework.web.multipart.MultipartFile;
 
 import com.epicbe.gestioneenergia.model.Comune;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CSVHelper {
-	  public static String TYPE = "text/csv";
-	  static String[] HEADERs = { "idProv", "idCom", "comune", "provincia" };
+	public static String TYPE = "text/csv";
+	// static String[] HEADERs = { "idProv", "idCom", "comune", "provincia" };
 
-	  public static boolean hasCSVFormat(MultipartFile file) {
+	public static boolean hasCSVFormat(MultipartFile file) {
 
-	    if (!TYPE.equals(file.getContentType())) {
-	      return false;
-	    }
+		if (!TYPE.equals(file.getContentType())) {
+			return false;
+		}
 
-	    return true;
-	  }
-
-	  public static List<Comune> csvToTutorials(InputStream is) {
-	    try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-	        CSVParser csvParser = new CSVParser(fileReader,
-	            CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
-
-	      List<Comune> comuni = new ArrayList<Comune>();
-
-	      Iterable<CSVRecord> csvRecords = csvParser.getRecords();
-
-	      for (CSVRecord csvRecord : csvRecords) {
-	    	  
-	    	Long idProv = Long.parseLong(csvRecord.get("idProv"));
-//	    	Long idCom = Long.parseLong(csvRecord.get("idCom"));
-	    	String comune = csvRecord.get("comune");
-	    	String provincia = csvRecord.get("provincia");
-	    	   	
-	    	Comune c = Comune.builder()
-//	    			.codiceComune(idCom)
-	    			.codiceProvincia(idProv)
-	    			.nome(comune)
-	    			.provincia(provincia)
-	    			.build();
-	    	
-	    	comuni.add(c);
-	      }
-
-	      return comuni;
-	    } catch (IOException e) {
-	      throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
-	    }
-	  }
-
+		return true;
 	}
+
+	public static List<Comune> csvToTutorials(MultipartFile file) {
+		try {
+			InputStream is = file.getInputStream();
+			BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			CSVParser csvParser = new CSVParser(fileReader, CSVFormat.EXCEL.withFirstRecordAsHeader());
+
+			List<Comune> comuni = new ArrayList<Comune>();
+
+			Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+			
+			log.info(csvRecords.toString());
+
+			for (CSVRecord csvRecord : csvRecords) {
+
+				Long idProv = Long.parseLong(csvRecord.get("idProv"));
+	    	Long idCom = Long.parseLong(csvRecord.get("idCom"));
+				String comune = csvRecord.get("comune");
+				String provincia = csvRecord.get("provincia");
+
+				Comune c = Comune.builder()
+	    			.codiceComune(idCom)
+						.codiceProvincia(idProv).nome(comune).provincia(provincia).build();
+
+				log.info("Ho un comune!!");
+				comuni.add(c);
+			}
+
+			csvParser.close();
+			return comuni;
+		} catch (IOException e) {
+			throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
+		}
+	}
+
+}
--- src/main/java/com/epicbe/gestioneenergia/service/CSVService.java
+++ src/main/java/com/epicbe/gestioneenergia/service/CSVService.java
@@ -11,16 +11,19 @@ import com.epicbe.gestioneenergia.helper.CSVHelper;
 import com.epicbe.gestioneenergia.model.Comune;
 import com.epicbe.gestioneenergia.repository.ComuneRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class CSVService {
 	
   @Autowired ComuneRepository repository;
 
   public void save(MultipartFile file) {
     try {
-      List<Comune> comuni = CSVHelper.csvToTutorials(file.getInputStream());
-      repository.saveAll(comuni);
-    } catch (IOException e) {
+      List<Comune> comuni = CSVHelper.csvToTutorials(file);
+     // repository.saveAll(comuni);
+    } catch (Exception e) {
       throw new RuntimeException("fail to store csv data: " + e.getMessage());
     }
   }
