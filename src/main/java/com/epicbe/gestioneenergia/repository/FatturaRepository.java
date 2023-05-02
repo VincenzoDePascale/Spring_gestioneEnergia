1a399f56c6ea50fca5d485253d858c672849fd10
Vincenzo Picone <118441474+Vincenzopicone@users.noreply.github.com>
2023-05-02 16:30:41 +0200

    Add Controller, Configuration, Service

--- src/main/java/com/epicbe/gestioneenergia/repository/FatturaRepository.java
+++ src/main/java/com/epicbe/gestioneenergia/repository/FatturaRepository.java
@@ -7,4 +7,8 @@ import com.epicbe.gestioneenergia.model.Fattura;
 
 public interface FatturaRepository extends CrudRepository<Fattura, Long>, PagingAndSortingRepository<Fattura, Long> {
 
+	public Fattura findByNumero(Integer numero);
+	public Boolean existByNumero(Integer numero);
+	public Fattura findByAnno(Integer anno);
+	public Boolean existByAnno(Integer anno);
 }
