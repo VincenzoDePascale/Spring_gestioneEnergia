b9c907bcf982b553886bd5e7b590c1f6f58319a0
Vincenzo Picone <118441474+Vincenzopicone@users.noreply.github.com>
2023-05-03 16:18:57 +0200

    Add pageable

--- src/main/java/com/epicbe/gestioneenergia/repository/FatturaRepository.java
+++ src/main/java/com/epicbe/gestioneenergia/repository/FatturaRepository.java
@@ -1,5 +1,9 @@
 package com.epicbe.gestioneenergia.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -10,5 +14,8 @@ public interface FatturaRepository extends CrudRepository<Fattura, Long>, Paging
 	public Fattura findByNumero(Integer numero);
 	public Boolean existsByNumero(Integer numero);
 	public Fattura findByAnno(Integer anno);
+	List<Fattura> findByAnnoList(Integer anno);
 	public Boolean existsByAnno(Integer anno);
+	Page<Fattura> findByNumero(Integer numero, Pageable page);
+	Page<Fattura> findByAnno(Integer anno, Pageable page);
 }
