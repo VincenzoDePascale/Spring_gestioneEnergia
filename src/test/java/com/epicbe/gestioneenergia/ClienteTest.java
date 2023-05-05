fa9d9b4c5c884dc46c3f61e95794755a78f314d6
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-05-05 11:33:40 +0200

    jUnit dependency issue

---
+++ src/test/java/com/epicbe/gestioneenergia/ClienteTest.java
@@ -0,0 +1,84 @@
+package com.epicbe.gestioneenergia;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteTest {
+
+	@org.junit.Test
+	void testClienteLongStringEClienteStringStringLocalDateLocalDateStringStringStringStringStringStringListOfIndirizzoListOfFattura() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetRagioneSociale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTipoCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPartitaIva() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetInserimento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUltimocontatto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPec() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTelefono() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEmailContatto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNomeContatto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCognomeContatto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTelefonoContatto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetIndirizzi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFatture() {
+		fail("Not yet implemented");
+	}
+
+}
